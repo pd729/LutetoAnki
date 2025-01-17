f8c882eb405f879aa3909aeb4009ad8d4805c9de
dvo729 <hipposhaman@proton.me>
2025-01-17 20:10:47 +0100

    Added variables for auto import and bug fixes

--- config.py
+++ config.py
@@ -2,29 +2,46 @@
 import json
 from aqt import mw
 import os
+from typing import Dict, Any
 from .logger import log_info, log_error
+
+defaults = {
+            'lutedb_path': 'Open file manager',
+            'parents_only': False,
+            'empty_translation': False,
+            'allow_duplicates': False,
+            'import_tags': False,
+            'selected_deck': "Default",
+            'selected_model': 'Basic',
+            'adjust_ease': False,
+            'include_WKI': False,
+            'selected_lang': 0,
+            'last_days': 0,
+            'tags': [],
+            'auto_import': False
+            }
 
 class Config:
     def __init__(self):
         """ Initializing addon directory and ID """
+        global defaults
         self.addon_dir = os.path.dirname(os.path.abspath(__file__))
         self.addon_id = os.path.basename(self.addon_dir)
-        self.default_config = {
-                'lutedb_path': 'Open file manager',
-                'parents_only': False,
-                'empty_translation': False,
-                'allow_duplicates': False,
-                'import_tags': False,
-                'selected_deck': 'Default',
-                'selected_model': 'Basic',
-                'adjust_ease': False,
-                'include_WKI': False,
-                'auto_import_on_startup': False
-                }
+        self.default_config = defaults
 
     # Get exisiting config or use the default one
-    def get_config(self):
-        return mw.addonManager.getConfig(self.addon_id) or self.default_config
+    def get_config(self) -> Dict[str, Any]:
+        """Get current config directly from config.json"""
+        config_path = os.path.join(self.addon_dir, 'config.json')
+        try:
+            with open(config_path, 'r') as file:
+                config = json.load(file)
+                log_info('Configuration loaded succesfully')
+        except (FileNotFoundError, json.JSONDecodeError):
+            # Fallback to default config if file not found or invalid
+            global defaults
+            config = defaults
+        return config
     
     def update_config(self, updates: dict) -> bool:
         """ Update config with new values while preserving existing ones """
@@ -54,6 +71,6 @@ class Config:
             conf = self.get_config()
             return conf.get(param)
         except Exception as e:
-            defaults = self.default_config
+            global defaults
             log_error(f'[config] Requested config parameter {param} not found, using default: {str(e)}')
             return defaults.get(param)  # Returns the default value for the param
